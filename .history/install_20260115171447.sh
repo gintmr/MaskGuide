f1534c60782c214b5a4ebe11ae50f9b63521e114
gintmr <w6x6r6@gmail.com>
2026-01-15 17:42:48 +0800

    rm .history/*

--- .history/install_20260115171447.sh
+++
@@ -1,2 +0,0 @@
-conda env create -f /data2/wuxinrui/Distill-SAM/environment.yaml
-pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cu124
--- .history/草稿_20251231101550.py
+++
@@ -1,3 +0,0 @@
-tensorboard --logdir /data2/wuxinrui/RA-L/MobileSAM/metrics_logs/Img_Encoder_copy_T_vit_t_S_tiny_msam/SAM_DISTILL --port=6013
-tensorboard --logdir /data2/wuxinrui/RA-L/MobileSAM/metrics_logs/Img_Encoder_T_vit_t_S_tiny_msam/SAM_DISTILL --port=6012
-tensorboard --logdir /data2/wuxinrui/RA-L/MobileSAM/metrics_logs/Img_Encoder_copy_copy_T_vit_t_S_tiny_msam/SAM_DISTILL --port=6010
